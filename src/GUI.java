953332a5d5f46ce2c98671f7818bfb71d2113806
Henry Andersson <henry.andersson@tuni.fi>
2020-12-23 14:43:11 +0200

    Made new layout

--- src/GUI.java
+++ src/GUI.java
@@ -16,15 +16,6 @@ import bets.Red;
 import bets.SecondDozen;
 import bets.SecondHalf;
 import bets.ThirdDozen;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
